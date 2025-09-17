00e18dfa853d3b26d3a28471b439df65252fad78
Даниил Карнаухов <Tyuiu.KarnaukhovDA@outlook.com>
2025-09-17 23:08:45 +0500

    Task1

---
+++ Tyuiu.KarnaukhovDA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World");
+        string name = "Даниил";
+        int age = 21;
+        Console.WriteLine("Здраствуйте, меня зовут " + name + " мне " + age + " лет!");
+        Console.ReadKey();
+    }
+}
